536448ce02ff79ed27752039650ef0027a882cfc
dekapaan <terblancheronald@gmail.com>
2021-05-06 23:34:28 +0200

    Task complete

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from tkinter import *
 from PIL import Image
+from tkinter import messagebox
+
 root = Tk()
 root.title("BMI Calculator")
 root.geometry("700x500")
 header = Label(root, text='Ideal Body Mass Index Calculator')
-header.place(relx=0, rely=0)
+header.place(x=250, y=0)
 
 frame = Frame(root, width=500, height=200, borderwidth=1, relief='ridge')
 frame.place(relx=0.15, rely=0.1)
@@ -33,6 +35,7 @@ variable.set(options[0])
 
 
 def activate(value):
+    variable.set(value)
     if value != "Select...":
         age_entry.config(state='normal')
     else:
@@ -43,22 +46,29 @@ gender_menu = OptionMenu(frame, variable, *options, command=activate)
 gender_menu.place(relx=0.3, rely=0.4)
 
 
-def bmi_calc(value):
-    result_bmi = weight / ((height / 100)**2)
-    bmi_field.config(state='normal')
-    bmi_field.insert(0, result_bmi)
-    bmi_field.config(state='readonly')
-    if value != "Select...":
-        if value == "Male":
-            result = ((0.5*weight) / ((height/100)**2)) + 11.5
+def bmi_calc():
+    try:
+        int(weight_entry.get())
+        int(height_entry.get())
+        int(age_entry.get())
+        if variable.get() == "Select...":
+            raise ValueError
+        elif variable.get() == "Male":
+            result = ((0.5 * int(weight_entry.get())) / ((int(height_entry.get()) / 100) ** 2)) + 11.5
+            result = round(result, 1)
             ideal_field.config(state='normal')
             ideal_field.insert(0, result)
             ideal_field.config(state='readonly')
-        elif value == "Female":
-            result = ((0.5 * weight) / ((height / 100)**2)) + (0.03 * age) + 11
+        elif variable.get() == "Female":
+            result = ((0.5 * int(weight_entry.get())) / ((int(height_entry.get()) / 100) ** 2)) + (
+                        0.03 * int(age_entry.get())) + 11
+            result = round(result, 1)
             ideal_field.config(state='normal')
             ideal_field.insert(0, result)
             ideal_field.config(state='readonly')
+    except ValueError:
+        messagebox.showerror(title=None, message='Gender was not specified or invalid entry was given')
+        delete()
 
 
 calculate = Button(root, text="Calculate your Ideal Body Mass Index", width=50, command=bmi_calc)
@@ -75,13 +85,23 @@ ideal_field.place(rely=0.7, relx=0.65)
 
 
 def delete():
-    height_entry.delete(0, END)
     weight_entry.delete(0, END)
+    height_entry.delete(0, END)
+    age_entry.config(state='normal')
+    bmi_field.config(state='normal')
+    ideal_field.config(state='normal')
+    age_entry.delete(0, END)
+    bmi_field.delete(0, END)
+    ideal_field.delete(0, END)
+    age_entry.config(state='readonly')
+    bmi_field.config(state='readonly')
+    ideal_field.config(state='readonly')
+    weight_entry.focus()
+    variable.set(options[0])
 
 
-clear = Button(root, text='Clear')
+clear = Button(root, text='Clear', command=delete)
 clear.place(rely=0.85, relx=0.1)
 quit = Button(root, text='Exit', command='exit')
 quit.place(rely=0.85, relx=0.83)
 root.mainloop()
-
